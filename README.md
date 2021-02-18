48d8341ae53cd0970f21b96b4186b113d9ccf91f
Shinjl <info@shinjl.com>
2021-02-18 19:57:13 +0900

    feat: split city list to other service

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# xForecast-server
+# xforecast-server
 
 <img align="right" width="64" height="64" src="./images/logo.png">
 
@@ -7,12 +7,21 @@ The backend service for xForecast, a simple weather forecast app built on Svelte
 ## Developing
 
 ```bash
+# create virtual environment and activate it
+virtualenv .venv
+source .venv/bin/activate
+
 # install dependencies
 pip install -r requirements.txt
 
 # configure environment variables
+# copy example config file and change variables
 cp .env.example .env
+# OPEN_WEATHERMAP_APP_KEY = Your OpenWeatherMap APP KEY (required)
+# IPSTACK_ACCESS_KEY = Your IPStack ACCESS KEY (required)
+# DEFAULT_LAT = Default latitude (optional)
+# DEFAULT_LON = Default longitude (optional)
 
 # start development server
-flask run
+.venv/bin/flask run
 ```
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request
 from markupsafe import escape
 import json
 import urllib.request
@@ -17,20 +17,7 @@ default_lon = os.environ.get("DEFAULT_LON")
 app = Flask(__name__)
 CORS(app)
 
-print('app started.')
-
-cities = {}
-# city.list.json from http://bulk.openweathermap.org/sample/city.list.json.gz
-with open('city.list.json') as city_list_file:
-    city_data = json.load(city_list_file)
-    for one in city_data:
-        cities[one['name'].lower()] = {
-            "name": one['name'],
-            "label": one['name'],
-            "id": one['id'],
-            "lat": one['coord']['lat'],
-            "lon": one['coord']['lon']
-        }
+print('xforecast-server service started')
 
 
 @app.route('/', methods=['GET'])
@@ -43,34 +30,17 @@ def index():
         result = json.loads(url.read().decode())
     print(result)
 
-    myLat = result['latitude']
-    myLon = result['longitude']
+    my_lat = result['latitude']
+    my_lon = result['longitude']
     if (result['latitude'] is None or result['longitude'] is None):
-        myLat = default_lat
-        myLon = default_lon
+        my_lat = default_lat
+        my_lon = default_lon
     return {
-        'forecast': fetch_data(myLat, myLon)
-    }
-
-
-@app.route('/cities/<name>', methods=['GET'])
-def get_cities(name):
-    city_name = urllib.parse.unquote(name)
-    found_cities = dict(
-        filter(lambda item: city_name in item[0], cities.items()))
-    sorted_cities = dict(
-        sorted(found_cities.items(), key=lambda item: item[0]))
-    return {
-        'cities': list(sorted_cities.values())
-    }
-
-
-@app.route('/forecast/<name>', methods=['GET'])
-def get_forecast_by_city(name):
-    city_name = urllib.parse.unquote(name)
-    target_city = cities[city_name]
-    return {
-        'forecast': fetch_data(target_city['lat'], target_city['lon'])
+        'forecast': fetch_data(my_lat, my_lon),
+        'location': {
+            'lat': my_lat,
+            'lon': my_lon
+        }
     }
 
 
